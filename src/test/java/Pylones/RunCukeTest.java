f2b1fd58f6018f09182308bd2cf1cbb83b54f5a3
chp026 <chp026@hotmail.com>
2020-06-28 18:22:49 +0100

    Testing afetr Delete

--- src/test/java/Pylones/RunCukeTest.java
+++ src/test/java/Pylones/RunCukeTest.java
@@ -8,8 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features = "C:\\Users\\Chirag\\IdeaProjects\\Pylones\\src\\test\\Resource\\SignIn.feature",tags = {"@regression"},
 plugin = {"html:target/Cucumber-html-report",
           "json:target/cucumber-report.json",
-          "junit:traget/cucumber-xml-report.xml",
-"pretty:target/cucumber-pretty-report.txt"}
+          "junit:traget/cucumber-xml-report.xml" }
 )
 
 
