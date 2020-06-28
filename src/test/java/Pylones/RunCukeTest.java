9cc73a064719035e585001fb28dabc04c9ba02ea
chp026 <chp026@hotmail.com>
2020-06-28 17:12:12 +0100

    Report Plugin

--- src/test/java/Pylones/RunCukeTest.java
+++ src/test/java/Pylones/RunCukeTest.java
@@ -5,7 +5,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\Chirag\\IdeaProjects\\Pylones\\src\\test\\Resource\\SignIn.feature",tags = {"@regression"})
+@CucumberOptions(features = "C:\\Users\\Chirag\\IdeaProjects\\Pylones\\src\\test\\Resource\\SignIn.feature",tags = {"@regression"},
+plugin = {"html:target/cucumber-report"})
 
 public class RunCukeTest {
 }
