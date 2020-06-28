fd9e50c41b7157da13f5ae69d4bf4c1644ad8178
chp026 <chp026@hotmail.com>
2020-06-28 17:45:33 +0100

    Trying Auto Run

--- src/test/java/Pylones/RunCukeTest.java
+++ src/test/java/Pylones/RunCukeTest.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "C:\\Users\\Chirag\\IdeaProjects\\Pylones\\src\\test\\Resource\\SignIn.feature",tags = {"@regression"},
 plugin = {"html:target/Cucumber-html-report",
-            "json:target/cucumber-report.json" }
+          "json:target/cucumber-report.json",
+          "junit:traget/cucumber-xml-report.xml"}
 )
 
 
