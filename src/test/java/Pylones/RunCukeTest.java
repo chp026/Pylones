57426dc9dbd7e03c6f54c19d2b36ccffb8491bce
chp026 <chp026@hotmail.com>
2020-06-28 17:38:07 +0100

    Reports

--- src/test/java/Pylones/RunCukeTest.java
+++ src/test/java/Pylones/RunCukeTest.java
@@ -7,9 +7,12 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "C:\\Users\\Chirag\\IdeaProjects\\Pylones\\src\\test\\Resource\\SignIn.feature",tags = {"@regression"},
 plugin = {"html:target/Cucumber-html-report",
-            "json:target/cucumber-report.json"
-}
+            "json:target/cucumber-report.json" }
 )
+
+
+
+
 
 public class RunCukeTest {
 }
